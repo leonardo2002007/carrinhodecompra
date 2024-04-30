f27abde130da17701b6ea6d167813405af250ae5
Leonardo Lima Da Silva <161377457+leonardo2002007@users.noreply.github.com>
2024-04-30 21:00:19 +0000

    carrinho

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho de Compras</title>
+</head>
 
+<body>
+   <?php
+    require("carrinho.php");
+
+    $sql = "SELECT * FROM produtos";
+    $qr = mysql_query($sql) or die (mysql_error());
+    while ($in = mysql_fetch_assoc($qr))(
+        echo '<h2>'.$in['nome']'<h2> <br />';
+        echo 'Preço : R$'.number_format($in)['preco'], 2, ',', '.');
+        echo '<img src="image/'.$in['imagem'].'" /> <br />;
+        echo '<a href="#carrinho.php?acao=add&id='.$in['id'].'"></a>';
+        echo '<br /><hr />';
+    )
+</body>
+</html>
